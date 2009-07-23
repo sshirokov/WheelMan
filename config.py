420d82f88ceaf8045f8ac964273523f65d06eeec
Yaroslav Shirokov <yshirokov@slavasaur.coxnewsajc.int>
2009-07-23 11:41:07 -0400

    Config now holds server/nick/channel info

--- config.py
+++ config.py
@@ -2,4 +2,9 @@ import os
 
 DEBUG = True
 
+SERVER_HOST = 'localhost'
+SERVER_PORT = 6669
+NICK = "WheelMan"
+CHANNEL = "#botworld"
+
 DATABASE="sqlite:///%s/db.db" % os.path.dirname(__file__)
--- wheelman.py
+++ wheelman.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from wheelman.core.handler import Handler
+import wheelman.settings as settings
 import routes
 
 def main():
-    bot = Handler("#botworld", "WheelMan", "localhost", 6669)
+    bot = Handler(settings.CHANNEL,
+                  settings.NICK,
+                  settings.SERVER_HOST,
+                  settings.SERVER_PORT)
     bot.start()
 
 if __name__ == "__main__":
